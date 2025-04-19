a4ea835310a276cfb31be55538503a46d3938987
janhavi-08 <pjanhavi71@gmail.com>
2025-04-19 21:47:34 +0530

    Create table in the DB

---
+++ OutfitGenAPI/Authentication.API/OutfitGen.Authenticate.API/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+namespace OutfitGen.Authenticate.API.Models
+{
+    public class User
+    {
+        public int Userid {  get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+        [Required, MaxLength (250)]
+        public string Email { get; set; }
+        [Required]
+        public string PasswordHash { get; set; }
+        public DateTime CreatedAt { get; set; } 
+        public DateTime LastUpdatedAt { get; set; } 
+        public ICollection<ClothingItem> ClothingItems { get; set; }
+ 
+
+    }
+}
